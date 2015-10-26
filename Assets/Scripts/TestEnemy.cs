04387b1c8d83bf3e0d1caf1f3ba1b7fb296e1e51
thetylerace13 <tylergemora@yahoo.com>
2015-10-25 18:36:31 -0700

    Added a test enemy

---
+++ Assets/Scripts/TestEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEnemy : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    { 
+        public static Transform playerObject;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        TestEnemy.playerObject = GameObject.FindGameObjectWithTag(Player);
+
+        transform.LookAt(TestEnemy.playerObject);
+
+        transform.translate(Vector3.forward*Time.deltaTime);
+	}
+}
