bee8e70b370596ff47278b41373487364544f0ec
jetsontwo <jetsontwo@gmail.com>
2015-11-03 22:39:05 -0800

    Added movement script for player as well as placeholders for player sprites. Also placed mana bar UI but it is not integrated to switch characters yet.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+  
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+        transform.Translate(inputVector*movementSpeed*Time.deltaTime);
+
+    }
+}
