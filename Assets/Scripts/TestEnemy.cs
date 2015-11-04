358f19720e9292deaa836751b84d3d1d8e39450c
jetsontwo <jetsontwo@gmail.com>
2015-11-03 19:36:40 -0800

    Fixed enemy tracking and created new 2D Unity scene

--- Assets/Scripts/TestEnemy.cs
+++ Assets/Scripts/TestEnemy.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class TestEnemy : MonoBehaviour {
 
+    public static GameObject playerObject;
+    public float speed;
+
     // Use this for initialization
     void Start()
-    { 
-        public static Transform playerObject;
+    {
+        
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        TestEnemy.playerObject = GameObject.FindGameObjectWithTag(Player);
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        transform.LookAt(playerObject.transform);
 
-        transform.LookAt(TestEnemy.playerObject);
-
-        transform.translate(Vector3.forward*Time.deltaTime);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed);
 	}
 }
