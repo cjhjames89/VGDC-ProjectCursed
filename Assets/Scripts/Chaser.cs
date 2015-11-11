ff8958ea857bc01d3eb9fcab73efe8df47dbd680
thetylerace13 <tylergemora@yahoo.com>
2015-11-10 17:58:55 -0800

    Began the chaser enemy, feel free to help with collision code.

---
+++ Assets/Scripts/Chaser.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chaser : MonoBehaviour {
+
+    public static float health;
+    public static float speed;
+    public static Transform player;
+    private Vector3 direction;
+    private Vector3 oppDirection;
+
+	// Use this for initialization
+	void Start ()
+    {
+        health = 10f;
+        speed = 4f;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        direction = new Vector3(player.position.x - gameObject.transform.position.x, player.position.y - gameObject.transform.position.y);
+
+        oppDirection = -direction;
+
+        direction.Normalize();
+
+        transform.Translate(direction * Time.deltaTime * speed);
+
+        if (direction.x < 0)
+        {
+            //Sprite faces one direction;
+        }
+        else if (direction.x > 0)
+        {
+            //Sprite faces other direction;
+        }
+
+        //If it hits player, bounce away.
+	}
+
+    IEnumerator BounceBack ()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.position - direction*3, ref oppDirection, 1);
+
+        yield return new WaitForSeconds(1);
+    }
+}
