d5388c50d4a9f6ca1dd4a9315d8d6d8ec96ee855
thetylerace13 <tylergemora@yahoo.com>
2015-11-12 15:16:51 -0800

    Started collider development and improved enemy sprite orientation.

--- Assets/Scripts/Chaser.cs
+++ Assets/Scripts/Chaser.cs
@@ -6,30 +6,55 @@ public class Chaser : MonoBehaviour {
     public static float health;
     public static float speed;
     public static Transform player;
-    private Vector3 direction;
+    private Vector3 direction1;
+    private Vector3 direction2;
+    private Vector3 move;
     private Vector3 oppDirection;
+    public Collider2D enemyCollider;
+    public Collider2D playerCollider;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         health = 10f;
         speed = 4f;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        oppDirection = -direction1;
+
+        enemyCollider = gameObject.GetComponent<Collider2D>();
+
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter(Collider2D playerCollider)
     {
-        direction = new Vector3(player.position.x - gameObject.transform.position.x, player.position.y - gameObject.transform.position.y);
+        StartCoroutine(BounceBack);
+    }
 
-        oppDirection = -direction;
+    // Update is called once per frame
+    void Update ()
+    {
+        direction1 = new Vector3(player.position.x - gameObject.transform.position.x, player.position.y - gameObject.transform.position.y, 0);
 
-        direction.Normalize();
+        move = direction1;
 
-        transform.Translate(direction * Time.deltaTime * speed);
+        direction2 = new Vector3(-direction1.x, direction1.y, direction1.z);
 
-        transform.LookAt(new Vector3(player.position.x, gameObject.transform.position.y));
+        direction1.Normalize();
+        direction2.Normalize();
+
+        if (move.x < 0)
+        {
+            transform.LookAt(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -1));
+
+            transform.Translate(direction2 * Time.deltaTime * speed);
+        }
+        else if (move.x > 0)
+        {
+            transform.LookAt(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1));
+
+            transform.Translate(direction1 * Time.deltaTime * speed);
+        }
 
         if (health <= 0)
         {
@@ -39,10 +64,9 @@ public class Chaser : MonoBehaviour {
         //If it hits player, bounce away.
 	}
 
-    
     IEnumerator BounceBack ()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, player.position - direction*3, ref oppDirection, 1);
+        transform.position = Vector3.SmoothDamp(transform.position, player.position - direction1*3, ref oppDirection, 1);
 
         yield return new WaitForSeconds(1);
     }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -3,25 +3,33 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
     public static float speed;
+    private float movement;
     public static float range;
     private Vector3 distance;
     public static float fireRate;
     private Vector3 player;
     private Vector2 move;
     public static float health;
+    public Collider2D enemyCollider;
+    public Collider2D playerCollider;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         health = 10f;
         range = 10f;
+        speed = 4f;
         fireRate = 1f;
         player = GameObject.FindGameObjectWithTag("Player").transform.position;
         distance = new Vector3(gameObject.transform.position.x - player.x, gameObject.transform.position.x - player.x);
         move = new Vector2(gameObject.transform.position.x - player.x, gameObject.transform.position.y - player.y);
 
         move.Normalize();
-	}
+
+        enemyCollider = gameObject.GetComponent<Collider2D>();
+
+        playerCollider = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider2D>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -31,7 +39,7 @@ public class Shooter : MonoBehaviour {
         /*
         if (distance.magnitude > range)
         {
-            speed = 4f;
+            movement = speed;
         }
         else
         {
