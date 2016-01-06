bd276209416b128fb1a8d815a3568745fbfe958d
Jiahao Chen <cjhjames89@gmail.com>
2016-01-05 21:30:43 -0800

    Fixed wall colliders bugs. Added trigger collider for the portal to be implemented.

--- Assets/Scripts/Characters Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Characters Scripts/Player/PlayerMovement.cs
@@ -5,17 +5,27 @@ public class PlayerMovement : MonoBehaviour {
 
     public float movementSpeed;
 
+    public LayerMask blockingLayer;
+
+    private BoxCollider2D boxCollider;
+
 	// Use this for initialization
 	void Start () {
-
+        boxCollider = GetComponent<BoxCollider2D>();
+        Debug.Log("x=" + boxCollider.size.x + " y=" + boxCollider.size.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        transform.Translate(inputVector*movementSpeed*Time.deltaTime);
-        
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
 
+        if (hInput == 0 && vInput == 0)
+            return;
+
+        Vector3 inputVector = new Vector2(hInput, vInput);
+
+        transform.Translate(inputVector * movementSpeed * Time.deltaTime);
     }
 
 }
