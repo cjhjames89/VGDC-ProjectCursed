b7f0988a3392525556f6b248a78e20f006f102a1
Jiahao Chen <cjhjames89@gmail.com>
2016-01-06 18:10:17 -0800

    Implemented the skeleton of room transition. Need to be polished later.

---
+++ Assets/Scripts/Levels/Portal.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+    public enum DoorLocation { North, East, South, West }
+
+    public DoorLocation doorLocation; 
+    public GameObject player;
+    public GameObject mainCamera;
+    public GameObject destinationRoom;
+    public GameObject destination;
+
+    private Portal destPortal;
+    private Vector3 playerDest;
+    private Vector3 cameraDest;
+    private Vector3 playerDestOffset;
+
+	// Use this for initialization
+	void Start () {
+        destPortal = destination.GetComponent<Portal>();
+	    switch (destPortal.doorLocation)
+        {
+            case DoorLocation.North:
+                playerDestOffset = new Vector3(0, 5, 0);
+                break;
+            case DoorLocation.East:
+                playerDestOffset = new Vector3(5, 0, 0);
+                break;
+            case DoorLocation.South:
+                playerDestOffset = new Vector3(0, -5, 0);
+                break;
+            case DoorLocation.West:
+                playerDestOffset = new Vector3(-5, 0, 0);
+                break;
+        }
+
+        playerDest = destination.transform.position + playerDestOffset;
+
+        cameraDest = new Vector3(destinationRoom.transform.position.x, destinationRoom.transform.position.y, -20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+        if (coll.transform.tag == "Player")
+        {
+            mainCamera.transform.position = cameraDest;
+            player.transform.position = playerDest;
+        }
+    }
+}
