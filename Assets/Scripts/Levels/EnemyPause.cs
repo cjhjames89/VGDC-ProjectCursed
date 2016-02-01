b88537ad64962718e89345ecca245e6332386b02
jetsontwo <jetsontwo@gmail.com>
2016-02-01 11:55:51 -0800

    Added a third level and added 10 levels, tried to make it more random and not as square looking

---
+++ Assets/Scripts/Levels/EnemyPause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPause : MonoBehaviour {
+    public GameObject enemy;
+    public Portal portal;
+    bool stayenabled = false;
+
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (portal.IsInvoking("print"))
+        {
+            print("testing");
+            stayenabled = !stayenabled;
+            print(stayenabled);
+        }
+        if (stayenabled)
+        {
+            enemy.transform.Translate(new Vector3(0, 0, 0));
+
+        }
+    }
+}
