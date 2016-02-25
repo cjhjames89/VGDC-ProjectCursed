6a0cadd9432e0d15c48ea5789e7328f28599dcb3
thetylerace13 <tylergemora@yahoo.com>
2016-02-24 16:53:55 -0800

    Started the shield enemy. Made a shield prefab and a script to go with it. Need to make enemy itself.

---
+++ Assets/Scripts/Characters/Enemy/EnemyShield.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShield : MonoBehaviour {
+    public float totalHealth;
+    private float health;
+
+    // Use this for initialization
+    void Start ()
+    {
+        health = totalHealth;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Transform player = GameObject.FindWithTag("Player").transform;
+        Transform target = player;
+        Vector3 targetPos = target.position;
+        targetPos.z = gameObject.transform.position.z;
+        target.position = targetPos;
+
+        transform.LookAt(target);
+
+        PublicFunctions.PhaseThruTag(gameObject, new string[] { "Enemy", "Danger" });
+    }
+
+    public void EnemyDamage(int damage)
+    {
+        health -= damage;
+    }
+}
