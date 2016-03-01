ab664cd19bb1fa352869f475f1cc60ef638fd6b5
jetsontwo <jetsontwo@gmail.com>
2016-02-29 18:23:10 -0800

    Attempting to make a suicide enemy that rushed towards the player and explodes, low healthbut very fast and annoying

---
+++ Assets/Scripts/Characters/Enemy/Suicide.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Suicide : MonoBehaviour {
+
+    public GameObject explosion;
+    public int damage;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D player)
+    {
+        if (player.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Explode());
+            PublicFunctions.DamageEnemy(player, damage);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        CommonEnemy norm = gameObject.GetComponent<CommonEnemy>();
+
+        if (norm.difference > 50)
+        {
+            transform.Translate(new Vector3(0, 0, 0));
+        }
+        else
+        {
+            transform.Translate(norm.direction * Time.deltaTime * norm.speed);
+        }
+    }
+
+    IEnumerator Explode()
+    {
+        explosion.SetActive(true);
+
+        yield return new WaitForSeconds(0.5f);
+
+        Destroy(gameObject);
+    }
+}
